61db43e39d9fbcac3087b55157746846e9f1847a
Deepanshu Malviya <deepanshumalviya8964@gmail.com>
2022-07-20 01:14:56 +0530

    login page basic setup

---
+++ src/components/HttpRequests.js
@@ -0,0 +1,6 @@
+export const login = (user) => {
+	console.log(user);
+};
+export const signUp = (user) => {
+	console.log(user);
+};
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,59 @@
+import { Grid, Paper, TextField, Button } from "@mui/material";
+import { useState } from "react";
+import "./login.css";
+import { login, signUp } from "../HttpRequests";
+
 const Login = () => {
-	return <div></div>;
+	const [cred, setCred] = useState({ username: "", password: "" });
+
+	function submit(event) {
+		event.preventDefault();
+		login(cred);
+	}
+
+	function handleInputChange(event) {
+		const name = event.target.name;
+		setCred((prevState) => {
+			return { ...prevState, [name]: event.target.value };
+		});
+		console.log(cred);
+	}
+
+	return (
+		<>
+			<Paper elevation={3} className="Paper">
+				<form onSubmit={submit}>
+					<Grid container spacing={2} className="GridOne">
+						<Grid item>
+							<TextField
+								id="username"
+								label="User Name"
+								name="username"
+								value={cred.username}
+								onChange={handleInputChange}
+								variant="outlined"
+							/>
+						</Grid>
+						<Grid item>
+							<TextField
+								id="password"
+								label="Password"
+								name="password"
+								value={cred.password}
+								onChange={handleInputChange}
+								variant="outlined"
+							/>
+						</Grid>
+						<Grid item>
+							<Button type="submit" variant="contained">
+								Submit
+							</Button>
+						</Grid>
+					</Grid>
+				</form>
+			</Paper>
+		</>
+	);
 };
 
 export default Login;
