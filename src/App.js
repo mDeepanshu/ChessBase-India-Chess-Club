f1ca3628ba5df0f621d933d83eb8b846e562b5e3
Deepanshu Malviya <deepanshumalviya8964@gmail.com>
2022-07-19 16:00:55 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import LoginComponent from "./components/Login/Login";
+
+function App() {
+	return (
+		<div className="App">
+			<LoginComponent></LoginComponent>
+		</div>
+	);
+}
+
+export default App;
